9294f0985abd931229ab6d0c2e0ab33c544d917a
michel kasanga <kasangamichel1@gmail.com>
2025-07-18 15:02:18 +0200

    exercice sur l'entrée au cinema

---
+++ Exercice/Billet.py
@@ -0,0 +1,6 @@
+def mineur():
+    return  8
+def majeur():
+        return  10
+def vieux():
+        return  7
---
+++ Exercice/condition.py
@@ -0,0 +1,53 @@
+import Billet
+
+def mineur(): #fonction pour l'ecart d'age
+   return  range(0, 18)
+
+def majeur(): #fonction pour l'ecart d'age
+   return  range(18, 60)
+
+def vieux(): #fonction pour l'ecart d'age
+   return  range(60, 100)
+
+def age(): # Fonction pour demander l'âge
+   return  input("Quel est votre age ? ")
+
+def popCorn():
+   return  5
+
+
+
+def cimema():
+    print("Bienvenue au cinéma !")
+    age = int(age())
+    if age in mineur():
+        pop = input("voulez vous du pop corn ? ")
+
+        if pop == "oui" or pop == "Oui" or pop == "OUI" or pop == "o" or pop == "O" or pop == "yes" or pop == "Y":
+            print(f"Votre billet coûte {Billet.mineur()} euros et votre pop corn {popCorn()} euros.\n Total : {Billet.mineur() + popCorn()} euros.")
+        else:
+            print(f"Votre billet coûte {Billet.mineur()} euros.")
+
+    elif age in majeur():
+        pop = input("voulez vous du pop corn ? ")
+
+        if pop == "oui" or pop == "Oui" or pop == "OUI" or pop == "o" or pop == "O" or pop == "yes" or pop == "Y":
+            print(f"Votre billet coûte {Billet.majeur()} euros et votre pop corn {popCorn()} euros.\n Total : {Billet.majeur() + popCorn()} euros.")
+        else:
+            print(f"Votre billet coûte {Billet.majeur()} euros.")
+
+    elif age in vieux():
+        pop = input("voulez vous du pop corn ? ")
+        
+        if pop == "oui" or pop == "Oui" or pop == "OUI" or pop == "o" or pop == "O" or pop == "yes" or pop == "Y":
+            print(f"Votre billet coûte {Billet.vieux()} euros et votre pop corn {popCorn()} euros.\n Total : {Billet.vieux() + popCorn()} euros.")
+        else:
+            print(f"Votre billet coûte {Billet.vieux()} euros.")
+    else:
+        print("Vous n'êtes pas autorisé à entrer dans le cinéma.")
+        
+
+if __name__ == "__main__":
+    cimema()
+
+    
